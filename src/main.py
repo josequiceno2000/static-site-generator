1bd370bde185f1ce354d6cec63969a565ed8a147
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-03-13 15:58:09 -0500

    Test both functions

--- src/main.py
+++ src/main.py
@@ -50,12 +50,6 @@ def main():
     my_node = TextNode("This is text with a `code block` word", TextType.NORMAL_TEXT)
     print(split_nodes_delimiter([my_node], "`", TextType.CODE_TEXT))
 
-    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
-    print(extract_markdown_images(text))
-    # [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
-    
-    text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
-    print(extract_markdown_links(text))
 
 main()
 
--- src/test_main.py
+++ src/test_main.py
@@ -1,7 +1,7 @@
 import unittest
 import textnode
 import htmlnode
-from main import text_node_to_html_node
+from main import text_node_to_html_node, extract_markdown_images, extract_markdown_links
 
 class TestNodetoHTML(unittest.TestCase):
 
@@ -11,6 +11,16 @@ class TestNodetoHTML(unittest.TestCase):
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "Hello")
         self.assertEqual(html_node.props, None)
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links("This is text with a link [to boot dev](https://www.boot.dev)")
+        self.assertListEqual([("to boot dev", "https://www.boot.dev")], matches)
 
     
 
